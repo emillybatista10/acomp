397309a3be3192d3bc82d46d8b9162ca809ef3b9
Emilly  Batista <103061139+emillybatista10@users.noreply.github.com>
2022-06-14 16:51:20 +0000

    Ordenação

---
+++ bubble.py
@@ -0,0 +1,35 @@
+import numpy as np
+from time import process_time_ns as ns
+
+def sort(v):
+ for j in range(len(v)-1,0,-1): #(1)
+   for i in range(j): #(2)
+     if v[i]>v[i+1]: #(3)
+       t = v[i] #(4)
+       v[i] = v[i+1] #(5)
+       v[i+1] = t #(6)
+  
+def main (*args):  
+  v=np.random.randint(1,1000,1000)
+  s=sorted(v)
+  r=sorted (v, reverse=True)
+
+  n1= ns()
+  m = sort(v)
+  n2= ns()
+
+  n3= ns()
+  m = sort(s)
+  n4= ns()
+
+  n5= ns()
+  m = sort(r)
+  n6= ns()
+  
+  print (n2-n1)
+  print (n4-n3)
+  print (n6-n5)
+ 
+  
+if __name__=='__bubble__':
+  bublle()
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-print("hello")
+import bubble
+bubble.main()
